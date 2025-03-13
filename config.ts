dfc9a645f59817b16c9062a0725080e054df193f
Rex <ningzhigardenia@gmail.com>
2025-03-13 21:10:51 +0800

    chore: update package ID and mint cap ID for production environment

--- config.ts
+++ config.ts
@@ -1,7 +1,7 @@
-export const PACKAGE_ID = "0x402fe18262d64678863e4f339aa8c7fc096878df56ae1559b204c39f5315bc46"; 
+export const PACKAGE_ID = "0x8de124f2d6815d3803bddbce5d0d0d718e2a198d2477567ee7acc44b8cb10a8d"; 
 
 // Object IDs
 export const OBJECTS = {
-    MINT_CAP_ID: "0x6aa541feb46986d16a192dd039506dcd9ac4314cc8f345b07f4388d25ad43ce6", // MintCap object ID
+    MINT_CAP_ID: "0x403cd04e03fdede1332502d41e629f9f10dcaa0db9e4aa7f63a95cafbea00e7a", // MintCap object ID
     // 其他全局 object IDs...
 } as const;
